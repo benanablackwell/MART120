9d714a3e5de3151a84c4f08b42dce3a3fa2a43e3
benanablackwell <77306427+benanablackwell@users.noreply.github.com>
2021-04-12 17:44:15 -0600

    homework

--- Homework 13/sketch.js
+++ Homework 13/sketch.js
@@ -1,36 +1,32 @@
-// JavaScript source code
+// character
 var characterX = 100;
 var characterY = 100;
-// define the key codes for each letter
-var w = 87; 
-var s = 83;
-var a = 65;
-var d = 68;
 
-// x and y for a shape
+// wasd
+var w = 85;
+var s = 81;
+var a = 69;
+var d = 70;
+
+// shapes
 var shapeX = 30;
 var shapeY = 50;
-var shapeX2 = 80;
-var shapeY2 = 120;
+
 var shapeXs = [];
 var shapeYs = [];
 var diameters = [];
 
 var shapeXSpeeds = [];
 var shapeYSpeeds = [];
-var shapeXSpeed;
-var shapeYSpeed;
-var shapeX2Speed;
-var shapeY2Speed;
 
-// create a shape when the mouse is clicked
+// mouse click shape
 var mouseShapeX;
 var mouseShapeY;
-function setup()
-{
+
+function setup() {
     createCanvas(500, 600);
-    // get a random speed when the it first starts
-	  var x = 50;
+    // ranadom speed
+    var x = 50;
     var y = 50;
     var diameter = 25;
     for (var i = 0; i < 10; i++) {
@@ -41,15 +37,11 @@ function setup()
         diameters[i] = getRandomNumber(30);
     }
 
-    shapeXSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
-    shapeYSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
-    shapeX2Speed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
-    shapeY2Speed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
+    createCharacter(200, 350);
 }
 
-function draw()
-{
-    background(80,69,78);
+function draw() {
+     background(80,69,78);
     stroke(0);
     fill(0);
     // top border
@@ -61,162 +53,49 @@ function draw()
     // right upper border
     rect(width-10,0,10,height-50);
 
-    // exit message
-    textSize(16);
-    text("EXIT", width-50,height-50)
-
     //character
     fill(23,40,40);
     circle(characterX,characterY,25);
 
-    // handle the keys
-    if(keyIsDown(w))
-    {
-        characterY -= 10;   
-    }
-    if(keyIsDown(s))
-    {
-        characterY += 10;   
-    }
-    if(keyIsDown(a))
-    {
-        characterX -= 10;   
-    }
-    if(keyIsDown(d))
-    {
-        characterX += 10;   
-    }
-
-    // potential enemy
-    {
-       // draw the shape
-        circle(shapeX, shapeY, 10);
-        fill(13, 145, 14);
-    }
-    {
-        circle(shapeX2, shapeY2, 20);
-        fill(35, 67, 65);
-    } 
-
-    
-
-    // move the shape
-    shapeX += shapeXSpeed;
-    shapeY += shapeYSpeed;
-    shapeX2 += shapeX2Speed;
-    shapeY2 += shapeY2Speed; 
-
-    // check to see if the shape has gone out of bounds
-    if(shapeX > width)
-    {
-        shapeX = 0;
-    }
-    if(shapeX < 0)
-    {
-        shapeX = width;
-    }
-    if(shapeY > height)
-    {
-        shapeY = 0;
-    }
-    if(shapeY < 0)
-    {
-        shapeY = height;
-    }
-
-    // check to see if the character has left the exit
-    if(characterX > width && characterY > width-50)
-    {
-        fill(0);
-        stroke(5);
-        textSize(26);
-        text("You Win!", width/2-50, height/2-50);
-    }
-
-    // create the shape based on the mouse click
-    fill(120,130,140);
-    circle(mouseShapeX, mouseShapeY, 25);
-}
-
-function mouseClicked()
-{
-    mouseShapeX = mouseX;
-    mouseShapeY = mouseY;
-}
-function keyPressed() 
-{
-    if (keyCode === LEFT_ARROW) {
-        characterX -= 10;
-    }
-    else if (keyCode === RIGHT_ARROW) {
-        characterX += 10;
-    }
-    else if (keyCode === UP_ARROW) {
-        characterY -= 10;
-    }
-    else if (keyCode === DOWN_ARROW) {
-        characterY += 10;
-    }
-}
-
-
-
-function setup()
-{
-	
-    createCanvas(500, 600);
-    shapeXSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
-    shapeYSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
-    shapeX2Speed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
-    shapeY2Speed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
-}
-
-function draw()
-{
-	background(100,150,20);
 
     // call createBorders function
     createBorders();
 
-    // call createCharacter function
-    createCharacter();
-
     // call movePlayer function
     movePlayer();
 
-    // call drawMouseclick function
-    drawMouseclick();
+    // exit message
+    textSize(18);
+    text("Exit", width-470,height-25)
 
-    // call createEnemy function
-    createEnemy();
-
-    // call createRandoMmovment function
-    createRandomMovement();
+    // call createCharacter function
+    createCharacter();
 
     // call createWin function
     createWin();
-
-    // exit message
-    textSize(25);
-    text("Exit", width-470,height-25)
-for (var i = 0; i < shapeXs.length; i++) {
+    
+    // create shapes
+    for (var i = 0; i < shapeXs.length; i++) {
     {
         circle(shapeXs[0],shapeYs[0], diameters[0]);
-        fill(100,80,70)
+        fill(120,80,70)
         circle(shapeXs[1],shapeYs[1], diameters[1]);
-        fill(20,30,60)
+        fill(30,30,60)
         circle(shapeXs[2],shapeYs[2], diameters[2]);
-        fill(50,75,90)
+        fill(70,75,90)
         circle(shapeXs[3],shapeYs[3], diameters[3]);
-        fill(10,20,20)
+        fill(18,20,20)
         circle(shapeXs[4],shapeYs[4], diameters[4]);
         fill(80,100,70)
     }
         shapeXSpeeds[i] = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
         shapeYSpeeds[i] = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
-		  shapeXs[i] += shapeXSpeeds[i];
+    
+    // shape movement 
+        shapeXs[i] += shapeXSpeeds[i];
         shapeYs[i] += shapeYSpeeds[i];
-        // check to see if the shape has gone out of bounds
+
+    // check to see if the shape has gone out of bounds
         if (shapeXs[i] > width) {
             shapeXs[i] = 0;
         }
@@ -229,114 +108,11 @@ for (var i = 0; i < shapeXs.length; i++) {
         if (shapeYs[i] < 0) {
             shapeYs[i] = height;
         }
-}
+    }
    
-    // enemy shapes
-    {circle(shapeY2, shapeY, 10);
-        fill(50,25,10)
-    }
-    {circle(shapeX2, shapeY2, 20);
-        fill(40,50,20)
-    }
-
-    // shape 1 out of bounds
-    if(shapeX > width)
-    {
-        shapeX = 0;
-    }
-    if(shapeX < 0)
-    {
-        shapeX = width;
-    }
-    if(shapeY > height)
-    {
-        shapeY = 0;
-    }
-    if(shapeY < 0)
-    {
-        shapeY = height;
-    }
-
-    // shape 2 out of bounds
-    if(shapeX2 > width)
-    {
-        shapeX2=0
-    }
-    if(shapeX2<0)
-    {
-        shapeX2 = width; 
-    }
-    if(shapeY2==height)
-    {
-        shapeY2=0;
-    }
-    if(shapeY2<0)
-    {
-        shapeY2=height; 
-    }
-
-}
-
-function createEnemy()
-{
-    {circle(shapeX, shapeY, 10);
-        fill(50,25,10)
-    }
-    {circle(shapeX2, shapeY2, 20);
-        fill(40,50,20)
-    }
-}
-
-function createRandomMovement()
-{
-    shapeX += shapeXSpeed;
-    shapeY += shapeYSpeed;
-    shapeX2+= shapeX2Speed;
-    shapeY2+= shapeY2Speed;
-} 
-
-function createWin()
-{
-    if(characterY > width && characterX < width-500)
-    {
-        stroke(5);
-        fill(0);
-        textSize(50);
-        text("You Win!", width/2-20, height/2-25);
-    }
-}
-
-function mouseClicked()
-{
-    mouseShapeX = mouseX;
-    mouseShapeY = mouseY;
-}
-
-function createCharacter()
-{
-    fill(60,30,90);
-    circle(characterX,characterY,25);
-}
-function createCharacter(x, y) {
-    characterX = x;
-    characterY = y;
-}
-function createBorders()
-{
-    // top border
-    rect(0,0,width,10);
-    // left border
-    rect(0,0,10,height-50);
-    // bottom border
-    rect(0, height-10,width, 10);
-    // right upper border
-    rect(width-10,0,10,height); 
-}
-
-function drawMouseclick()
-{
-    fill(100,100,100);
-    circle(mouseShapeX, mouseShapeY, 30);
+    // create the shape based on the mouse click
+    fill(120, 130, 140);
+    circle(mouseShapeX, mouseShapeY, 25);
 }
 
 function movePlayer()
@@ -359,18 +135,40 @@ function movePlayer()
     }
 
 }
+function createCharacter()
+{
+    fill(60,30,90);
+    circle(characterX,characterY,25);
+}
 
-function keyPressed() {
-    if (keyCode === LEFT_ARROW) {
-        characterX -= 10;
-    } 
-    else if (keyCode === RIGHT_ARROW) {
-        characterX += 10;
+function createWin()
+{
+    if(characterY > width && characterX < width-500)
+    {
+        stroke(5);
+        fill(0);
+        textSize(50);
+        text("You Win!", width/2-20, height/2-25);
     }
-    else if (keyCode === UP_ARROW) {
-        characterY -= 10;
-    }
-    else if (keyCode === DOWN_ARROW) {
-        characterY += 10;
-	}
+}
+
+function createBorders()
+{
+    // top border
+    rect(0,0,width,10);
+    // left border
+    rect(0,0,10,height-50);
+    // bottom border
+    rect(0, height-10,width, 10);
+    // right upper border
+    rect(width-10,0,10,height); 
+}
+
+function mouseClicked() {
+    mouseShapeX = mouseX;
+    mouseShapeY = mouseY;
+}
+
+function getRandomNumber(number) {
+    return Math.floor(Math.random() * number) + 10;
 }
